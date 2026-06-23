13026d4b6d2bc6a8f1f99e42efe0ab49b0fc6bca
Mohamed Ayman <devel.mohamed.ayman@gmail.com>
2026-06-23 06:58:46 +0300

    feat: redesign login page layout with split-screen illustration and enhanced form styling

--- resources/views/livewire/advisor/auth/login.blade.php
+++ resources/views/livewire/advisor/auth/login.blade.php
@@ -1,53 +1,84 @@
-<div>
-    <!-- Logo -->
-    <div class="app-brand justify-content-center mb-6">
-        <a href="javascript:void(0);" class="app-brand-link">
-            <span class="app-brand-logo demo">
-                <span class="text-primary">
-                    <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z" fill="currentColor" />
-                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
-                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z" fill="currentColor" />
-                    </svg>
+<div class="row w-100 h-100 m-0 p-0">
+    <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
+        <div class="w-px-400 mx-auto mt-sm-12 mt-8">
+            <div class="text-center mb-6">
+                <span class="app-brand-logo demo mb-3">
+                    <span class="text-primary">
+                        <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                                fill="currentColor" />
+                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
+                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
                 </span>
-            </span>
-            <span class="app-brand-text demo text-heading fw-bold">بوابة المرشدين الأكاديميين</span>
-        </a>
-    </div>
-    <!-- /Logo -->
-    <h4 class="mb-1">مرحباً بك في بوابة المرشدين 👋</h4>
-    <p class="mb-6">يرجى تسجيل الدخول للوصول إلى طلابك وتسجيل المواد الخاصة بهم.</p>
+                <h3 class="mb-1 fw-bold">بوابة المرشدين الأكاديميين</h3>
+                <p class="mb-0 text-body">قم بتسجيل الدخول للوصول إلى طلابك وتسجيل المواد الخاصة بهم</p>
+            </div>
 
-    <form wire:submit.prevent="login" class="mb-4">
-        <div class="mb-6">
-            <label for="username" class="form-label">اسم المستخدم</label>
-            <input type="text" class="form-control" id="username" wire:model.defer="username" placeholder="أدخل اسم المستخدم" autofocus>
-            @error('username') <span class="text-danger mt-1 small">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-6 form-password-toggle">
-            <label class="form-label" for="password">كلمة المرور</label>
-            <div class="input-group input-group-merge">
-                <input type="password" id="password" class="form-control" wire:model.defer="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                <span class="input-group-text cursor-pointer"><i class="ti tabler-eye-off"></i></span>
-            </div>
-            @error('password') <span class="text-danger mt-1 small">{{ $message }}</span> @enderror
-        </div>
-        <div class="my-8">
-            <div class="d-flex justify-content-between">
-                <div class="form-check mb-0 ms-2">
-                    <input class="form-check-input" type="checkbox" id="remember-me" wire:model.defer="remember">
-                    <label class="form-check-label" for="remember-me">
-                        تذكرني
-                    </label>
+            <form wire:submit="login">
+                <div class="mb-5">
+                    <label for="login-username" class="form-label">اسم المستخدم (كود المرشد)</label>
+                    <div class="input-group input-group-merge">
+                        <span class="input-group-text"><i class="ti tabler-user"></i></span>
+                        <input type="text" wire:model="username" class="form-control @error('username') is-invalid @enderror"
+                               id="login-username" placeholder="أدخل اسم المستخدم" autofocus>
+                    </div>
+                    @error('username')
+                        <div class="invalid-feedback d-block">{{ $message }}</div>
+                    @enderror
                 </div>
-            </div>
+
+                <div class="mb-5">
+                    <label for="login-password" class="form-label">كلمة المرور</label>
+                    <div class="input-group input-group-merge" x-data="{ showPassword: false }">
+                        <span class="input-group-text"><i class="ti tabler-lock"></i></span>
+                        <input :type="showPassword ? 'text' : 'password'" wire:model="password"
+                               class="form-control @error('password') is-invalid @enderror"
+                               id="login-password" placeholder="············">
+                        <span class="input-group-text cursor-pointer" @click="showPassword = !showPassword">
+                            <i class="ti" :class="showPassword ? 'tabler-eye' : 'tabler-eye-off'"></i>
+                        </span>
+                    </div>
+                    @error('password')
+                        <div class="invalid-feedback d-block">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-5 d-flex justify-content-between flex-wrap gap-2">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" wire:model="remember" id="remember-me">
+                        <label class="form-check-label" for="remember-me">تذكرني</label>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary d-grid w-100" wire:loading.attr="disabled">
+                    <span wire:loading.remove>
+                        <i class="ti tabler-login me-1"></i> تسجيل الدخول
+                    </span>
+                    <span wire:loading>
+                        <span class="spinner-border spinner-border-sm me-1" role="status" aria-hidden="true"></span>
+                        جاري التحقق...
+                    </span>
+                </button>
+            </form>
         </div>
-        <div class="mb-6">
-            <button class="btn btn-primary d-grid w-100" type="submit" wire:loading.attr="disabled">
-                <span wire:loading.remove wire:target="login">تسجيل الدخول</span>
-                <span wire:loading wire:target="login">جاري الدخول...</span>
-            </button>
+    </div>
+
+    <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-12 pb-2">
+        <div class="text-center">
+            <img src="{{asset('assets/img/illustrations/auth-login-illustration-light.png')}}"
+                 class="authentication-image-model d-none d-lg-block"
+                 alt="auth-illustration"
+                 data-app-light-img="illustrations/auth-login-illustration-light.png"
+                 data-app-dark-img="illustrations/auth-login-illustration-dark.png"
+                 width="500">
         </div>
-    </form>
+    </div>
 </div>
